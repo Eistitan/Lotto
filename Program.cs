da63ac5a8281eb5c7007807221011009831e7111
Dietrich Kirsch <eistitan@yahoo.com>
2022-10-05 11:17:31 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//ZahlenArrayErstellung
+using System;
+
+int[] zahlen = new int[49];
+
+for (int i = 0; i < 49; i++)
+{
+    zahlen[i] =(i+1);
+}
+
+//foreach (var item in zahlen)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine("\n\n");
+
+
+//ZahlenZiehung
+Random rnd = new Random();
+int[] lottozahlen = new int[6];
+int counter = 48;
+
+for (int i = 0; i < 6; i++)
+{
+    var rndIndex = rnd.Next(0, counter);
+    lottozahlen[i] = zahlen[rndIndex];
+    zahlen =zahlen.Where((val, idx) => idx != rndIndex).ToArray();
+    counter--;
+}
+
+Array.Sort(lottozahlen);
+Console.WriteLine("Die Lottozahlen sind: \n");
+
+
+foreach (var item in lottozahlen)
+{
+    Console.WriteLine(item);
+}
+int rndSupp = rnd.Next(0, 9);
+Console.WriteLine($"\nDie Superzahl ist {rndSupp}.");
+
+//Console.WriteLine("\n\n");
+
+//foreach (var item in zahlen)
+//{
+//    Console.WriteLine(item);
+//}
+Console.WriteLine("\nLeider schon wieder verloren. ");
